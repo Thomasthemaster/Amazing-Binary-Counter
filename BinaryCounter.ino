870f738141382b86fb89779f9f8b1357b48a8246
Thomasthemaster <mingenmybaby@gmail.com>
2026-07-28 10:27:06 -0700

    Refactor prompt messages and improve comments
    Updated prompt messages for clarity and added comments for better understanding.

--- BinaryCounter.ino
+++ BinaryCounter.ino
@@ -1,16 +1,16 @@
-int latchPin = 11;
-int clockPin = 9;
-int dataPin = 12;
+const int latchPin = 11;
+const int clockPin = 9;
+const int dataPin = 12;
 int delayTime;
 
 byte LEDs = 0x00;
 
 void prompt() {
-  Serial.println("Please type in the delay time you want.");
-  while (Serial.available() == 0) {}
-  delayTime = Serial.parseInt();
+  Serial.println("Please type in the delay time you want. (in milliseconds)");
+  while (Serial.available() == 0) {} // Do nothing while waiting for the user to enter the delay time
+  delayTime = Serial.parseInt(); // By using Serial.parseInt(), we only read the integer part of the answer
   if (delayTime < 1) {
-    delayTime = 1;
+    delayTime = 1; // Ensures that delay time is at least 1 millisecond
   }
 }
 void setup() {
@@ -19,18 +19,18 @@ void setup() {
   pinMode(latchPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
-  prompt();
+  prompt(); // Prompt first for the delay time
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.print("LEDs = ");
-  Serial.println(LEDs, BIN);
+  Serial.println(LEDs, BIN); // Prints LED Sequence in Binary to the Serial Monitor
   digitalWrite(latchPin, LOW);
   shiftOut(dataPin, clockPin, LSBFIRST, LEDs);
   digitalWrite(latchPin, HIGH);
   if (LEDs == 0xFF) {
-    prompt();
+    prompt(); // Re-prompt if LED Cycle is finished (11111111)
   }
   LEDs++;
   delay(delayTime);
